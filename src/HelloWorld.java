e31f977c02f196d5300406ba186379875dccb968
wangshuai <350332528@qq.com>
2017-09-29 15:52:15 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by wangshuai on 2017/9/29.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("the first idea project");
+    }
+}
