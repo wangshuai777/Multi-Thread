ddeafcd3dba77a2c3e0607392259d2fe920633de
wangshuai <350332528@qq.com>
2018-02-01 11:21:00 +0800

    testCommand

--- src/Application.java
+++ src/Application.java
@@ -5,6 +5,7 @@ public class Application {
     public static void main(String[] args){
         System.out.println("the first git test..");
         System.out.println("the second git test..");
+        System.out.println("test git stash....");
 
     }
 }
